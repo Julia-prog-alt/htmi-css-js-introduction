80376855217b6d23b68254c09a1c83960f6e9b91
Julia <julia09071993@gmail.com>
2022-11-06 20:16:15 +0200

    CW#15

--- app2.js
+++ app2.js
@@ -28,4 +28,65 @@ let arCodeAsci = arStr.map(function(symbol, index){
 Метод charCodeAt() возвращает числовое значение Юникода для символа по указанному индексу
 */
 
+console.log(arStr, arCodeAscii);
+let sumAscii = arStr.reduce(function (res, cur){
+    return res + cur.charCodeAt();
 
+}, 0)
+console.log("sum of ascii ", sumAscii)
+console.log(arStr.reduce(function(res, cur) {
+return res + cur
+}, ""));
+function getNumberOddIndex(element){
+    let res = element * 2;
+    if (res > 9 ){
+        res -= 9;
+    }
+    return res;
+}
+
+function getCurrentNumber(element, index){
+    return index % 2 == 0 ? +element : getNumberOddIndex(element);  
+}
+function getArrayForSum(teudatStrNumber) {
+    let array = array.from(teudatStrNumber);
+   ;
+    return array.map(getyCurrentNumber);
+}
+getSum(array) {
+ //   let res = 0;
+ //   for(let i = 0; i<array.length; i++){
+  //      res += array[i];
+ 
+return array.reduce(function(res, cur) {
+}, 0);
+}
+function checkTeudatZehut(teudatStrNumber) {
+    //TODO
+    //control sum of for even index digit value, for odd index digit * 2
+    //control sum should be divide on 10 with no remainder
+    //example 123456782 => 1 + 4 +3 + 8 +5 + 3 + 7 + 7 + 2 => true
+    //    123456783 => 1 + 4 +3 + 8 +5 + 3 + 7 + 7 + 3 => false
+   let res = false;
+    if(teudatStrNumber.length !=9 || isNaN(teudatStrNumber)){
+        let arrayForSum = getArrayForSum(teudatStrNumber);
+let sum = getSum(arrayForSum);
+res = sum % 10 == 0;
+    }
+    return res;
+}
+
+
+    function generateRandomTeudatZehut() {
+        //TODO
+        //returns string of 9 symbols matching checkTeudatZehut
+        //make 8 random digits from 0 to 9
+        //9 - th symbol should be with accordance of matching
+        //to get random digit Math.round(Math.random() * 9)
+    }
+
+//console.log(checkTeudatZehut(`123456783`));
+['123456782', '1234', 'abcd123', '123456783']
+numbers.forEach(function(e) {
+    console.log(`teudat zeut: ${e}, return of the mehod checkTeudatZeut: $(checkTeudatZeut(e))`)
+});
